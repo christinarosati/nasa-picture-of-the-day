c117c791b1efc6735eff80675c09522d285a276f
christinarosati <chrisrosati22@gmail.com>
2022-11-15 16:41:35 -0500

    Integrated API returns and styled the app

--- src/App.js
+++ src/App.js
@@ -4,18 +4,19 @@ import PictureOfTheDay from "./PictureOfTheDay";
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Astronomy Picture of the Day</h1>
-        <p>
-          Discover the cosmos! Each day a different image or photograph of our
-          fascinating universe is featured, along with a brief explanation
-          written by a professional astronomer.
-        </p>
-      </header>
-      <main>
-        <PictureOfTheDay />
-      </main>
-      <footer>Coded by Christina Rosati</footer>
+      <div className="container">
+        <header>
+          <h1>Astronomy Picture of the Day</h1>
+          <p>
+            Discover the cosmos! Each day a different image or photograph of our
+            fascinating universe is featured, along with a brief explanation
+            written by a professional astronomer.
+          </p>
+        </header>
+        <main>
+          <PictureOfTheDay />
+        </main>
+      </div>
     </div>
   );
 }
--- src/PictureOfTheDay.js
+++ src/PictureOfTheDay.js
@@ -1,17 +1,18 @@
 import React, { useState } from "react";
 import axios from "axios";
+import "./PictureOfTheDay.css";
 
 export default function PictureOfTheDay() {
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
 
   function handleResponse(response) {
-    console.log(response.data);
     setResults(response.data);
     setLoaded(true);
   }
 
   function handleClick(event) {
+    event.preventDefault();
     let apiKey = "DVhrO5OaJqd6Etlwgh6U5lE0RfUMykBXhb2V37av";
     let apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}`;
     axios.get(apiUrl).then(handleResponse);
@@ -20,7 +21,14 @@ export default function PictureOfTheDay() {
   if (loaded) {
     return (
       <div className="PictureOfTheDay">
-        <h2>{results.date}</h2>
+        <p className="date">{results.date}</p>
+        <p className="title">{results.title}</p>
+        <img src={results.url} alt={results.title} />
+        <p className="copyright">
+          Image Credit & Copyright: {results.copyright}
+        </p>
+        <p className="explanation">{results.explanation}</p>
+        <small>Open-sourced code by Christina Rosati</small>
       </div>
     );
   } else {
