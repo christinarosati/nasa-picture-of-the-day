01f4ace7bc7606af08620ea215ca1c9182e54406
christinarosati <chrisrosati22@gmail.com>
2022-11-15 14:30:38 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Astronomy Picture of the Day</h1>
+        <p>
+          Discover the cosmos! Each day a different image or photograph of our
+          fascinating universe is featured, along with a brief explanation
+          written by a professional astronomer.
+        </p>
+      </header>
+      <main></main>
+      <footer>Coded by Christina Rosati</footer>
+    </div>
+  );
+}
+
+export default App;
